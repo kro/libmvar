e26346fbfb68371b104241549dc2fe38e02e9b2e
kro <karim.osman@helsinki.fi>
2009-07-02 01:14:30 +0300

    test mvar_fit()
    mvar_fit() is tested by first generating a process following a
    multivariate AR(p) model and then estimating the parameters. Once the
    parameters are estimated, they care compared with the actual parameters
    of the data generating process.

--- test.c
+++ test.c
@@ -1,7 +1,11 @@
 #include "mvar.h"
 #include "mvardebug.h"
+#include "mvartest.h"
+#include "mvardie.h"
 
-#define NR_STEPS 10
+#include <math.h>
+
+#define NR_STEPS 1e+4
 #define MVAR_ORDER 2
 
 static gsl_matrix *coef_mat_A(void)
@@ -42,18 +46,38 @@ static gsl_vector *intercept_terms(void)
 
 int main(int argc, char *argv[])
 {
+    struct mvar_model est_model;
     struct mvar_model model;
     gsl_matrix *v;
     gsl_rng *rng;
+    double sbc;
+    int p;
 
     model.A = coef_mat_A();
     model.C = cov_noise_mat_C();
     model.w = intercept_terms();
 
+    /* Generate a process following a multivariate AR(2) model. */
     rng = gsl_rng_alloc(gsl_rng_ranlxs0);
     v = mvar_sim(&model, NR_STEPS, rng);
 
-    mvar_print_matrix(v);
+    /* Estimate the parameters of the generated process. */
+    p = 2;
+    est_model.A = gsl_matrix_alloc(MVAR_ORDER, MVAR_ORDER * 2);
+    est_model.C = gsl_matrix_alloc(MVAR_ORDER, MVAR_ORDER);
+    est_model.w = gsl_vector_alloc(MVAR_ORDER);
+
+    mvar_fit(v, p, &est_model, &sbc);
+    /* The intercept vector is proportional to the actual one. */
+    gsl_vector_scale(est_model.w, 0.5);
+
+    assert_vec_equals(est_model.w, model.w, 1e-1);
+    assert_mat_equals(est_model.A, model.A, 1e-2);
+    assert_mat_equals(est_model.C, model.C, 1e-2);
+
+    gsl_matrix_free(est_model.A);
+    gsl_matrix_free(est_model.C);
+    gsl_vector_free(est_model.w);
 
     gsl_rng_free(rng);
     gsl_matrix_free(v);
